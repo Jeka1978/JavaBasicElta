9ecd0773eb74ccea7b13a73afdc16b6eac8dc852
jeka1978 <kit2009>
2022-07-06 01:11:32 -0500

    lab never use switch

---
+++ src/main/java/com/elta/never_use_switch_lab/DistributionService.java
@@ -0,0 +1,8 @@
+package com.elta.never_use_switch_lab;
+
+/**
+ * @author Evgeny Borisov
+ */
+public interface DistributionService {
+    void sendMessage(Message message);
+}
---
+++ src/main/java/com/elta/never_use_switch_lab/Message.java
@@ -0,0 +1,9 @@
+package com.elta.never_use_switch_lab;
+
+/**
+ * @author Evgeny Borisov
+ */
+public class Message {
+    private String text;
+    private int deliveryType;
+}
