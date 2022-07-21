79ee99c2f300bf884a010ba895c527de5208f1ae
jeka1978 <kit2009>
2022-07-21 06:40:42 -0500

    fix init method should run

--- src/main/java/com/elta/my_spring/CleanerImpl.java
+++ src/main/java/com/elta/my_spring/CleanerImpl.java
@@ -7,6 +7,12 @@ public class CleanerImpl implements Cleaner {
     @InjectRandomInt(min = 3, max = 6)
     private int repeat;
 
+    //todo make this method run in the beginning, but after object was configured and all was injected
+
+    public void init() {
+        System.out.println("repeat = " + repeat);
+    }
+
     @Override
     public void clean() {
         for (int i = 0; i < repeat; i++) {
--- src/main/java/com/elta/my_spring/IRobot.java
+++ src/main/java/com/elta/my_spring/IRobot.java
@@ -12,7 +12,6 @@ public class IRobot {
     @InjectByType
     private Cleaner cleaner;
 
-    //todo make it work @InjectRandomName(fileName = "path to file")
     private String name;
 
     public void cleanRoom() {
